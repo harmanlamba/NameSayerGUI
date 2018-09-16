99e69b7676d3971aafb4a1ddfbdfcc8ead826f0e
Harman Lamba <harrygth15478@gmail.com>
2018-09-16 22:09:40 +1200

    Made it so the 2 labels are responsive, such that they can also be resized.

--- src/NameSayer/Main.java
+++ src/NameSayer/Main.java
@@ -12,7 +12,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../ControllersAndFXML/MainSceneFoenix.fxml"));
-        Font.loadFont(Main.class.getResource("../icons/fa-regular-400.ttf").toExternalForm(), 10);
+        Font font=Font.loadFont(getClass().getResource("../icons/ionicons.ttf").toExternalForm(), 10);
+        System.out.println(font);
         //System.out.println(Font.getFontNames());
         primaryStage.setTitle("NameSayer");
         primaryStage.setScene(new Scene(root, 981, 553));
