d3ded045f4beef79c795c4feb451e648cc42ec70
Harman Lamba <harrygth15478@gmail.com>
2018-09-18 20:23:02 +1200

    Implemented the Playback slider and the main buttons in the Lower HBox

--- src/ControllersAndFXML/Controller.java
+++ src/ControllersAndFXML/Controller.java
@@ -1,23 +1,32 @@
 package ControllersAndFXML;
 
 
-import NameSayer.Main;
+
 import NameSayer.backend.CreationStore;
+import NameSayer.backend.Recording;
+import com.jfoenix.controls.JFXSlider;
+import javafx.animation.PauseTransition;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Modality;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -28,12 +37,16 @@ public class Controller implements Initializable {
 
     @FXML
     public Button playButton;
+    public Button nextButton;
+    public Button previousButton;
+    public Button recordButton;
     public ComboBox comboBox;
     public Label topLabel;
     public Label bottomLabel;
     public HBox bottomLabelHBox;
     public HBox mediaControlHBox;
     public CreationsListView listView;
+    public JFXSlider playbackSlider;
 
     public Controller(CreationStore creationStore) {
         _creationStore = creationStore;
@@ -44,26 +57,125 @@ public class Controller implements Initializable {
         bottomLabelHBox.setMouseTransparent(false);
         mediaControlHBox.setMouseTransparent(false);
         bottomLabel.setMouseTransparent(false);
-        comboBox.getItems().addAll("Baboons","Soajsdlkfasd");
+        comboBox.getItems().addAll("Baboons", "Soajsdlkfasd");
         listView.setCreationStore(_creationStore);
     }
 
-    public void recordButton() throws IOException {
+    public void recordButtonAction() throws IOException {
         //Parent recordingScene = FXMLLoader.load(getClass().getResource("/ControllersAndFXML/RecordingBox.fxml"));
         Stage recordingWindow = new Stage();
-        FXMLLoader loader= new FXMLLoader(getClass().getResource("/ControllersAndFXML/RecordingBox.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ControllersAndFXML/RecordingBox.fxml"));
         //Important to note that we have a place-holder for the creationName...
         loader.setController(new RecordingBox(recordingWindow, "creationName"));
-        Parent recordingScene= loader.load();
+        Parent recordingScene = loader.load();
         recordingWindow.initModality(Modality.APPLICATION_MODAL);
         recordingWindow.setResizable(false);
         recordingWindow.setTitle("Recording Box");
-        recordingWindow.setScene(new Scene(recordingScene, 600 , 168));
+        recordingWindow.setScene(new Scene(recordingScene, 600, 168));
         recordingWindow.show();
         recordingScene.requestFocus();
-        recordingWindow.setOnHidden(e ->{
+        recordingWindow.setOnHidden(e -> {
             topLabel.requestFocus();
         });
     }
+
+    public void playButtonAction() {
+        //If concatenation has to be done
+        //TODO
+
+        //Just Playing already existing files
+        String filePath;
+        StringBuilder concatenatedFileName = new StringBuilder();
+        ObservableList<Recording> selectedCreations = listView.getSelectedRecordings();
+
+        for (Recording counter : selectedCreations) {
+            concatenatedFileName.append(counter.getCreation().getName());
+        }
+
+        if (selectedCreations.size() > 1) {
+            filePath = "./data/attempts/" + concatenatedFileName;
+        } else {
+            filePath = "./data/attempts/" + concatenatedFileName;
+        }
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                mediaLoaderAndPlayer("./data/attempts/se206_2-5-2018_15-23-50_Mason.wav");
+            }
+        });
+        thread.start();
+    }
+
+    public void nextButtonAction() {
+       Thread thread= new Thread (new Runnable() {
+           @Override
+           public void run() {
+               mediaLoaderAndPlayer(listView.selectNext());
+           }
+       });
+    }
+
+    public void previousButtonAction(){
+        Thread thread= new Thread (new Runnable() {
+            @Override
+            public void run() {
+              mediaLoaderAndPlayer(listView.selectPrevious());
+            }
+        });
+    }
+
+
+    public void mediaLoaderAndPlayer(String filePath) {
+
+        PauseTransition delay= new PauseTransition(Duration.seconds(5));
+        Media media = new Media(new File(filePath).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        playbackSlider.setMax(mediaPlayer.getTotalDuration().toSeconds());
+        mediaPlayer.play();
+        delay.play();
+        recordButton.setDisable(true);
+        previousButton.setDisable(true);
+        playButton.setDisable(true);
+        nextButton.setDisable(true);
+
+        delay.setOnFinished(e -> {
+            recordButton.setDisable(false);
+            previousButton.setDisable(false);
+            playButton.setDisable(false);
+            nextButton.setDisable(false);
+        });
+
+        //configuring the playback slider
+
+        mediaPlayer.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+            @Override
+            public void changed(ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue) {
+                playbackSlider.setValue(newValue.toSeconds());
+            }
+        });
+
+        playbackSlider.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                mediaPlayer.seek(Duration.seconds(playbackSlider.getValue()));
+            }
+        });
+
+    }
+
+    //Could potentially use. But going based on the assumption that concatenated files will be saved in attempts
+    public boolean creationTypeChecker(ObservableList<Recording> selectedCreations) {
+        int size = selectedCreations.size();
+        boolean sameType = true;
+        for (Recording counter : selectedCreations) {
+            if (counter.getType() == Recording.Type.ATTEMPT) {
+                sameType = false;
+            }
+        }
+        return sameType;
+    }
+
 }
 
