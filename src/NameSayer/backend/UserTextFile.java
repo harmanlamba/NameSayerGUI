887d57df80cf9ba321b56e84602421e915e507f3
Harman Lamba <harrygth15478@gmail.com>
2018-10-04 00:22:50 +1300

    Implemented UserTextFile, which reads input from user file and puts it all in a list

---
+++ src/NameSayer/backend/UserTextFile.java
@@ -0,0 +1,43 @@
+package NameSayer.backend;
+
+
+import javafx.stage.FileChooser;
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserTextFile {
+    ArrayList<String> _finalListOfNames = new ArrayList<String>();
+
+    public List readFile() {
+        FileChooser fc = new FileChooser();
+        FileChooser.ExtensionFilter extensionFilter= new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fc.getExtensionFilters().addAll(extensionFilter);
+        File selectedFile = fc.showOpenDialog(null);
+        if (selectedFile != null) {
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(selectedFile));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    for (String name : line.split(" ")) {
+                        if (!name.equals("")) {
+                            name.replaceAll("[\\-]", "");
+                            _finalListOfNames.add(name);
+                        }
+                    }
+                    _finalListOfNames.add("\n");
+                    System.out.println("Debugging Dump: " + _finalListOfNames);
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return _finalListOfNames;
+    }
+
+
+}
