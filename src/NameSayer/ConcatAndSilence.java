3189161367d4b7243f68d3177a6e2b257788cf2e
Harman Lamba <harrygth15478@gmail.com>
2018-10-05 14:16:11 +1300

    Added code to normalise each file before concatenating, and then deleting tempCreations folder

--- src/NameSayer/ConcatAndSilence.java
+++ src/NameSayer/ConcatAndSilence.java
@@ -22,6 +22,7 @@ import NameSayer.backend.Recording;
  */
 public abstract class ConcatAndSilence {
     private static final String DEFAULT_TEMP_FOLDER = "tempPlayback";
+    private static final String DEAFULT_TEMP_CREATIONS_FOLDER="data/tempCreations/";
 
     public abstract void ready(String filePath);
 
@@ -48,14 +49,18 @@ public abstract class ConcatAndSilence {
             private final List<Recording> _recordings = new ArrayList<>(recordings);
 
             @Override
-            protected Void call() throws Exception {
+            protected Void call(){
 
                 List<String> concatData = new ArrayList<String>();
+                List<Path> creationsPaths=new ArrayList<>();
 
                 //Setting up the correct format for the file, and iterating through all the selected recordings, and adding
                 //them to the concatData arrayList to then have it be written into a file.
                 for (Recording counter : _recordings) {
-                    concatData.add("file '../../" + counter.getPath() + "'");
+                    //concatData.add("file '../../" + counter.getPath() + "'");
+                    concatData.add("file '../../"+DEAFULT_TEMP_CREATIONS_FOLDER + counter.getPath().getFileName() + "'");
+                    System.out.println("ConcatData: "+concatData);
+                    creationsPaths.add(counter.getPath());
                 }
 
                 Path path = Paths.get("./data/" + folder + "/concat.txt");
@@ -66,6 +71,25 @@ public abstract class ConcatAndSilence {
                     e.printStackTrace();
                     // TODO
                 }
+                //Normalising the audio files before concatenating them
+                for(Path counter:creationsPaths){
+                    //Setting up the process builder
+                    String cmd="ffmpeg -i "+"./"+counter.toString()+" -af_loudnorm=I=-16:TP=-1.5:LRA=11:measured_I=-27.2:measured_TP=-14.4:measured_LRA=0.1:measured_thresh=-37.7:offset=-0.7:linear=true"+" ./data/tempCreations/"+counter.getFileName();
+                    System.out.println(cmd);
+                    ProcessBuilder nomalizeBuilder= new ProcessBuilder("/bin/bash","-c", cmd);
+                    try {
+                        Process processNormalize = nomalizeBuilder.start();
+                        processNormalize.waitFor();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+
+
                 //Setting up the ProcessBuilders to execute the bash commands which concatenate and silence the recordings
                 //appropriately
                 ProcessBuilder concatBuilder = new ProcessBuilder("/bin/bash", "-c",
@@ -78,11 +102,15 @@ public abstract class ConcatAndSilence {
                         "-i ./data/" + folder + "/playBack.wav -af " +
                         "silenceremove=1:0:-50dB:1:5:-50dB:0 " +
                         "./data/" + folder + "/playBackSilenced.wav");
+                ProcessBuilder deleteTempCreations= new ProcessBuilder("/bin/bash","-c",
+                    "rm -r ./data/tempCreations");
                 try {
                     Process process = concatBuilder.start();
                     process.waitFor(); //ensuring that concatenation happens before silencing
                     Process processSilence = silenceRemoverBuilder.start();
                     processSilence.waitFor();
+                    Process deleteTempCreationsProcess= deleteTempCreations.start();
+                    deleteTempCreationsProcess.waitFor();
                 } catch (IOException e) {
                     e.printStackTrace();
                 } catch (InterruptedException e) {
