10a88583f4a3f6bf7fea5e72151fe1e0b96b58f8
Ernest Wong <sudo@ernestwong.nz>
2018-10-06 21:17:26 +1300

    Minor: tidy whitespace for StreaksAndTiers

--- src/NameSayer/StreaksAndTiers.java
+++ src/NameSayer/StreaksAndTiers.java
@@ -17,44 +17,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StreaksAndTiers {
-    private static final String STREAKS_FILENAME ="streaks.dat";
-    private static final Path _path= Paths.get("./data");
+
+    private static final String STREAKS_FILENAME = "streaks.dat";
+    private static final Path _path = Paths.get("./data");
+
     private CreationStore _creationStore;
-    InvalidationListener changesInStreakIntegerProperty= o -> {
+    private InvalidationListener changesInStreakIntegerProperty = o -> {
         saveStreaks();
     };
 
-    public StreaksAndTiers(CreationStore creationStore){
-        _creationStore=creationStore;
+    public StreaksAndTiers(CreationStore creationStore) {
+        _creationStore = creationStore;
         updateListeners();
         _creationStore.addListener(e -> {
             updateListeners();
         });
     }
 
-
-    private synchronized void saveStreaks(){
-        List<String> streakData= new ArrayList<String>();
-        List<Creation> creationList= _creationStore.getCreations();
-        for(Creation creation: creationList){
-            streakData.add(creation.getName() +"\t"+ creation.getStreaks());
+    private synchronized void saveStreaks() {
+        List<String> streakData = new ArrayList<String>();
+        List<Creation> creationList = _creationStore.getCreations();
+        for (Creation creation: creationList) {
+            streakData.add(creation.getName() + "\t" + creation.getStreaks());
         }
         try {
-            Files.write(_path.resolve(STREAKS_FILENAME),streakData, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+            Files.write(_path.resolve(STREAKS_FILENAME), streakData, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void updateListeners(){
-        for(Creation creation: _creationStore.getCreations()){
+    private void updateListeners() {
+        for (Creation creation: _creationStore.getCreations()) {
             creation.getStreaksProperty().removeListener(changesInStreakIntegerProperty);
             creation.getStreaksProperty().addListener(changesInStreakIntegerProperty);
         }
-
     }
-
-
-
 
 }
